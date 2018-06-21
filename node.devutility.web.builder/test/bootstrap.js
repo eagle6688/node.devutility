895e38175cae0d3bbfa12aca972ec8817ea74f69
Aldwin Su <fangyansu@163.com>
2018-06-21 16:18:07 +0800

    Test the web-builder

--- node.devutility.web.builder/test/bootstrap.js
+++ node.devutility.web.builder/test/bootstrap.js
@@ -11,6 +11,39 @@ let config = {
             'resources/scripts/bootstrap.min.js',
             'resources/scripts/vue.min.js'
         ]
+    },
+    server: {
+        router: function (app) {
+            app.get('/', function (request, response, next) {
+                response.redirect('/index');
+            });
+
+            app.get('/login', function (request, response, next) {
+                let data = {};
+                data.requireAuth = false;
+                response.render("pages/login/index", data);
+            });
+
+            app.get('/index', function (request, response, next) {
+                let data = {};
+                data.requireAuth = true;
+                response.render("pages/index/index", data);
+            });
+
+            // Catch api and 404 request.
+            app.use(function (request, response, next) {
+                var error = new Error('Not Found');
+                error.status = 404;
+                next(error);
+            });
+
+            // Error handler
+            app.use(function (err, req, res, next) {
+                console.log("express error: ", err);
+                res.status(err.status || 500);
+                res.render('error', { title: 'Not Found', layout: false });
+            });
+        }
     }
 };
 
