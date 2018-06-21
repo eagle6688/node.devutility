009a535699bf349fbec58e9ec911d0782faeb3b6
Aldwin Su <fangyansu@163.com>
2018-06-21 13:47:29 +0800

    add methods about url in config-helper.js

--- node.devutility.web.builder/lib/config-helper.js
+++ node.devutility.web.builder/lib/config-helper.js
@@ -9,7 +9,7 @@ function Helper(options) {
     this.options = options;
 }
 
-// File name
+/* File name */
 
 Helper.prototype.getPageStyleName = function (page) {
     return this.options.pages.styleNameFormat.replace(/{page}/, page);
@@ -19,9 +19,9 @@ Helper.prototype.getPageScriptName = function (page) {
     return this.options.pages.scriptNameFormat.replace(/{page}/, page);
 };
 
-// File name end
+/* File name end */
 
-// Path
+/* Path */
 
 Helper.prototype.getStyleDirectory = function () {
     return sysPath.join(projectDirectory, this.options.deploy.stylesDir);
@@ -41,7 +41,32 @@ Helper.prototype.getPageScriptPath = function (page) {
     return sysPath.join(projectDirectory, this.options.deploy.scriptsDir, fileName);
 };
 
-// Path end
+/* Path end */
+
+/* Url */
+
+Helper.prototype.staticUrls = function () {
+    let array = [];
+    array.push("/" + this.options.deploy.fontsDir);
+    array.push("/" + this.options.deploy.imagesDir);
+    array.push("/" + this.options.deploy.stylesDir);
+    array.push("/" + this.options.deploy.scriptsDir);
+    return array;
+};
+
+Helper.prototype.pageStyleUrl = function (page) {
+    return "/" + this.options.deploy.stylesDir + "/" + this.getPageStyleName(page);
+};
+
+Helper.prototype.scriptLibUrl = function () {
+    return "/" + this.options.deploy.scriptsDir + "/" + this.options.deploy.scriptsLibName;
+};
+
+Helper.prototype.pageScriptUrl = function (page) {
+    return "/" + this.options.deploy.scriptsDir + "/" + this.getPageScriptName(page);
+};
+
+/* Url end */
 
 module.exports = function (options) {
     return new Helper(options);
