ca58d05ce0e8cbd432c37d6975580f2e55f990fc
Aldwin Su <fangyansu@163.com>
2018-06-22 10:13:43 +0800

    Update node.devutility.forwarder

--- node.devutility.forwarder/lib/helper.js
+++ node.devutility.forwarder/lib/helper.js
@@ -11,14 +11,8 @@ const httpProxy = require("http-proxy");
 const querystring = require("querystring");
 const defaults = require("./defaults");
 
-function Helper(options, debug) {
+function Helper(options) {
     this.options = extend(true, {}, defaults, options);
-    this.debug = false;
-
-    if (debug) {
-        this.debug = debug;
-    }
-
     this.init();
 }
 
@@ -26,58 +20,58 @@ Helper.prototype.init = function () {
     /**
      * Create a proxy server.
      */
-    let proxy = httpProxy.createProxyServer(this.options);
+    this.proxy = httpProxy.createProxyServer(this.options);
 
     /**
      * Listen for the `proxyReq` event on `proxy`.
      */
-    proxy.on('proxyReq', function (proxyReq, request, response, options) {
-        console.log('Event proxyReq was triggered for request url ' + request.url);
-
-        if ((/post/i.test(request.method) || /patch/i.test(request.method)) && request.body) {
-            var bodyData = request.body;
-            var contentType = request.headers['content-type'];
-            console.log("contentType: " + contentType);
-
-            if (typeof (bodyData) == 'string') {
-                forwardData(proxyReq, bodyData);
-                return;
-            }
-
-            if (contentType) {
-                if (contentType.indexOf('application/json') > -1) {
-                    forwardData(proxyReq, JSON.stringify(bodyData));
-                    return;
-                }
-
-                if (contentType.indexOf('application/x-www-form-urlencoded') > -1) {
-                    forwardData(proxyReq, querystring.stringify(bodyData));
-                    return;
-                }
-            } else {
-                forwardData(proxyReq, querystring.stringify(bodyData));
-                return;
-            }
-        }
-    });
+    this.proxy.on('proxyReq', this.proxyReq);
 
     /**
      * Listen for the `proxyRes` event on `proxy`.
      */
-    proxy.on('proxyRes', function (proxyRes, req, res) {
+    this.proxy.on('proxyRes', function (proxyRes, req, res) {
         console.log('Response from the target', JSON.stringify(proxyRes.headers, true, 4));
     });
 
     /**
      * Listen for the `error` event on `proxy`.
      */
-    proxy.on('error', function (err, req, res) {
+    this.proxy.on('error', function (err, req, res) {
         console.log(err);
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('System error, this message comes from node.js server!');
     });
+};
 
-    this.proxy = proxy;
+Helper.prototype.proxyReq = function (proxyReq, request, response, options) {
+    console.log('Event proxyReq was triggered for request url:', request.url);
+
+    if ((/post/i.test(request.method) || /patch/i.test(request.method)) && request.body) {
+        var bodyData = request.body;
+        var contentType = request.headers['content-type'];
+        console.log("contentType:", contentType);
+
+        if (typeof (bodyData) == 'string') {
+            forwardData(proxyReq, bodyData);
+            return;
+        }
+
+        if (contentType) {
+            if (contentType.indexOf('application/json') > -1) {
+                forwardData(proxyReq, JSON.stringify(bodyData));
+                return;
+            }
+
+            if (contentType.indexOf('application/x-www-form-urlencoded') > -1) {
+                forwardData(proxyReq, querystring.stringify(bodyData));
+                return;
+            }
+        } else {
+            forwardData(proxyReq, querystring.stringify(bodyData));
+            return;
+        }
+    }
 };
 
 Helper.prototype.request = function (request, response) {
