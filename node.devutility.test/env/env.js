c2808fb5d20e5d88350d26e2dda016df84841aed
Aldwin Su <fangyansu@163.com>
2018-07-02 12:05:05 +0800

    Add env.js

---
+++ node.devutility.test/env/env.js
@@ -0,0 +1,6 @@
+console.log(process.env["$HOST"]);
+console.log(process.env["$HOSTNAME"]);
+console.log(process.env["HOST"]);
+console.log(process.env["HOSTNAME"]);
+console.log(process.env["NODE_APP_INSTANCE"]);
+console.log(process.env["$NODE_ENV"]);
