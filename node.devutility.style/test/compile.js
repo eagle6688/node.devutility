2104494a283e4dc2aa532b8cd61892d3a2f2ea7b
Aldwin Su <fangyansu@163.com>
2020-06-26 19:14:33 +0800

    Upgrade version to 1.0.1 and update test cases.

--- node.devutility.style/test/compile.js
+++ node.devutility.style/test/compile.js
@@ -3,16 +3,25 @@ const path = require("path");
 const styleUtilies = require("../index");
 const nodeSass = require('node-sass');
 
-let inputFile = "";
+let inputFile = path.resolve('test/resources/main.scss');
 let outputCss1 = path.resolve('test/output/test1.css');
 let outputCss2 = path.resolve('test/output/test2.css');
 
 nodeSass.render({
     file: inputFile
 }, function (error, result) {
-    fs.writeFile(outputCss1, result.css);
+    fs.writeFile(outputCss1, result.css, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!', outputCss1);
+    });
 });
 
 let result = styleUtilies.compile(inputFile);
+
+fs.writeFile(outputCss2, result.css, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!', outputCss1);
+});
+
 console.log(result.stats);
-fs.writeFile(outputCss2, result.css);
+console.log("Completed!");
